205e2a8d356272a6050c9cf841f2065bc318295c
Josh Santos <josh@omnidapps.com>
2020-01-18 14:36:18 +0700

    Use var with dollar sign

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ IFS=$'\n'
 CHANGED_DIRS=$(git diff --name-only $TRAVIS_COMMIT_RANGE | cut -d"/" -f1 | uniq)
 IFS=$OLD_IFS
 for element in $CHANGED_DIRS; do
-  cd element; ./build.sh
+  if [ -d "$element" ]; then
+    cd $element; ./build.sh
+  fi
 done
