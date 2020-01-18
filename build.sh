9dbfe95b0772672cd29ea3221a4735fcace4fbf6
Josh Santos <josh@omnidapps.com>
2020-01-18 15:37:43 +0700

    [BUILD ALL] - Add ability to build all

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
-OLD_IFS=$IFS
-IFS=$'\n'
-CHANGED_DIRS=$(git diff --name-only $TRAVIS_COMMIT_RANGE | cut -d"/" -f1 | uniq)
-IFS=$OLD_IFS
-for element in $CHANGED_DIRS; do
-  if [ -d "$element" ]; then
+set -x
+BUILD_ALL_REGEX="BUILD ALL"
+if [[ $TRAVIS_COMMIT_MESSAGE =~ $BUILD_ALL_REGEX ]]; then
+  DIRS=$(find . -maxdepth 1 -type d -not -name ".git" -not -name ".")
+else
+  OLD_IFS=$IFS
+  IFS=$'\n'
+  DIRS=$(git diff --name-only "$TRAVIS_COMMIT_RANGE" | cut -d"/" -f1 | uniq)
+  IFS=$OLD_IFS
+fi
+
+for element in $DIRS; do
+  if [[ -d "$element" && -f "$element/build.sh" ]]; then
     cd $element; ./build.sh
   fi
 done
